dfdee0ce5e89d05552d7d025a8a228b9ca26a09d
Crafter-Y <mrcrafter.yt1@gmail.com>
2026-08-10 22:25:12 +0200

    update base image and nginx

--- scripts/local-test.sh
+++ scripts/local-test.sh
@@ -30,7 +30,7 @@ fi
 # --- Build -------------------------------------------------------------------
 echo "==> Building addon image..."
 docker build \
-    --build-arg BUILD_FROM=ghcr.io/hassio-addons/base:21.0.0 \
+    --build-arg BUILD_FROM=ghcr.io/hassio-addons/base:21.0.1 \
     -t "$IMAGE_NAME" \
     "${PROJECT_DIR}/homebox"
 
